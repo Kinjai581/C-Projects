8eefdf2ca01160e816efa6824d3e38601453a4d8
Vejhay Prakash <125725410+Kinjai581@users.noreply.github.com>
2024-02-03 08:59:25 +1100

    Update reverse charArray.c with correction for line 10

--- reverse charArray.c
+++ reverse charArray.c
@@ -7,7 +7,8 @@ int main() {
         //So you can do the equivalent and get the same result: sizeof(char_array) - 1. If you are dealing with different types of character encodings of different sizes (such as UTF-16), then it's better
         //to divide by the size of the char primitive type i.e: sizeof(char_array)/sizeof(char_array[0]) - 1. **
         
-        size_t length = sizeof(char_array)/sizeof(char)-1; //Calculates the length of the string
+        //size_t length = sizeof(char_array)/sizeof(char)-1; //Calculates the length of the string
+        size_t length = sizeof(char_array)/sizeof(char_array[0]) -1;
 
         char *pointer = char_array + length - 1; // Declare a pointer to char and initialize it to the addr>
 
